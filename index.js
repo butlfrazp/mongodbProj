3648a03652999c1e784d71de8979384d169eec22
butlfrazp <paulfbutler2016@gmail.com>
2018-03-27 16:40:19 -0700

    update

--- index.js
+++ index.js
@@ -50,113 +50,51 @@ const client = new Client({
 });;
 
 app.listen(PORT, () => {
-  console.log(`Started up on Port ${PORT}`);
+
 });
 
 app.get("/myaccount", (req, res) => {
-  account(client, accounts => {
-    res.send(accounts);
-  });
+
 });
 
 app.get("/myaccount/balance", (req, res) => {
-  account(client, accounts => {
-    var balance = [];
-    accounts.map(amount => {
-      const data = {
-        amount: amount.balance.amount,
-        currency: amount.balance.currency
-      };
-      balance.push(data);
-    });
-    res.send(balance);
-  });
+
 });
 
 app.post("/sell", (req, res) => {
-  sell(req, res, client);
+
 });
 
 app.post("/buy", (req, res) => {
-  buy(req, res, client);
+
 });
 
 app.get("/portfolio", (req, res) => {
-  var data = {};
-  const params = "price quantity symbol"
-  Sell.find({}, params, (err, sell) => {
-    if (err) {
-      res.status(400).send(err);
-      return;
-    }
-    data.sell = sell;
-    Buy.find({}, params, (err, buy) => {
-      if (err) {
-        res.status(400).send(err);
-        return;
-      }
-      data.buy = buy;
-      res.send(data);
-    })
-  })
+
 });
 
 app.get("/past_transactions", (req, res) => {
-  var data = {};
-  Sell.find({}, (error, sell) => {
-    if (error) {
-      res.staus(400).send(error);
-    }
-    data["sell"] = sell;
-    Buy.find({}, (error, buy) => {
-      if (error) {
-        res.staus(400).send(error);
-      }
-      data["buy"] = buy;
-      res.send(data);
-    })
-  })
-})
+
+});
 
 app.get("/currency", (req, res) => {
-  allCurrencies(req, res, client);
+
 });
 
 app.get("/currency/:country", (req, res) => {
-  currency(req, res, client);
+
 });
 
 app.get("/prices", (req, res) => {
-  getDefaultPrice(req, res, client);
+
 });
 
 app.get("/prices/:country", (req, res) => {
-  getPrice(req, res, client);
+
 });
 
 app.get("/holdings", (req, res) => {
-  var data = {};
-  Buy.find({}, (error, buy) => {
-    if (error) {
-      res.staus(400).send(error);
-    }
-    buy.map(elem => {
-      if (!data[elem.symbol]) {
-        data[elem.symbol] = elem.quantity
-      }else{
-        data[elem.symbol] += elem.quantity;
-      }
-    });
-    Sell.find({}, (error, sell) => {
-      if (error) {
-        res.staus(400).send(error);
-      }
-      sell.map(elem => {
-        data[elem.symbol] -= elem.quantity;
-      });
-      res.send(data);
-    });
-  });
+
 });
 
 app.get("/")
--- server/models/transaction.js
+++ server/models/transaction.js
@@ -2,24 +2,7 @@ const {mongoose} = require('./../DB/mongoose');
 var Schema = mongoose.Schema;
 
 const transaction = new Schema({
-  symbol: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  quantity: {
-    type: Number,
-    default: 1
-  },
-  date: {
-    type: Date,
-    requred: true
-  },
-  price: {
-    type: Number,
-    required: true
-  }
+
 });
 
 module.exports = {transaction};
